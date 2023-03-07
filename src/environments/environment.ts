271d2b7f58adbb36ed71386a93746626c855eb4a
sali2021am <sfifi2019@gmail.com>
2023-03-07 22:31:47 +0100

    commit3

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  keycloak:{
-      issuer :"http://localhost:8080/auth/realms/cyber_App2",
-      redirectUri:"http://localhost:4200/",
-      clientId:"AngularSchoolApp",
-      scope:"openid profile email offline_access"
-  }
-};
